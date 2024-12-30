c9937fc62506cd6ced8cb9c95ec07de5fba423d4
Murtada Altarouti <murtada.altarouti@gmail.com>
2024-12-30 21:38:15 +0300

    #6 validation doc strong

--- thumbny/models/validation/__init__.py
+++ thumbny/models/validation/__init__.py
@@ -11,7 +11,16 @@ from dataclasses import dataclass
 HEX_REGEX = r'^#[0-9a-fA-F]{6}$'
 
 
-def _is_optional(field_type) -> bool:
+def _is_optional(field_type: Any) -> bool:
+    """Helper to check the field if optional
+    based on the annotations
+
+    Args:
+        field_type (field type): field type
+
+    Returns:
+        bool: True if optional, otherwise False
+    """
     origin = get_origin(field_type)
     if origin is Union:
         return type(None) in get_args(field_type)
@@ -19,6 +28,14 @@ def _is_optional(field_type) -> bool:
 
 
 def check_required_fields(instance: dataclass) -> None:
+    """Check required fields of the given instance
+
+    Args:
+        instance (dataclass): dataclass model
+
+    Raises:
+        ValueError: value error if required field was not given
+    """
     annotations = get_type_hints(instance.__class__)
     for field_name, field_type in annotations.items():
         if (not _is_optional(field_type)
@@ -27,11 +44,29 @@ def check_required_fields(instance: dataclass) -> None:
 
 
 def check_spaces(field_name: str, field_value: Any) -> None:
+    """Check spaces if exist
+
+    Args:
+        field_name (str): field name
+        field_value (Any): field value
+
+    Raises:
+        ValueError: value error if spaces are exist
+    """
     if field_value.count(" "):
         raise ValueError(f"{field_name} cannot contain spaces")
 
 
 def check_hex_color(field_name: str, field_value: Any) -> None:
+    """Check hex color value
+
+    Args:
+        field_name (str): field name
+        field_value (Any): field value
+
+    Raises:
+        ValueError: value error if hex was not correct
+    """
     if not re.match(HEX_REGEX, str(field_value)):
         raise ValueError(f"{field_name} isn't correct. "
                          "Should follow this format: #FFFFFF "
