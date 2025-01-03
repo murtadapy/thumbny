e8b2f1f5b84eb90b2cbf968146d396b84dc20cff
Murtada-Altarouti <murtada.altarouti@gmail.com>
2025-01-03 21:32:03 +0300

    #6 template manager validator

--- thumbny/templates_manager/validator.py
+++ thumbny/templates_manager/validator.py
@@ -20,14 +20,32 @@ class Validator:
         self.templates_path = templates_path
 
     def validate_tempalate_key(self, name: str) -> None:
+        """Validate the template name
+
+        Args:
+            name (str): template name
+
+        Raises:
+            TemplateExist: raise template exist
+        """
         if name in self.template_manager.get_all_templates():
             raise TemplateExist(f"{name} template already exists")
 
     def validate_font_family(self, font_family: Optional[str]) -> None:
+        """validate font family
+
+        Args:
+            font_family (Optional[str]): font family path
+
+        Raises:
+            FontNotFound: font not found
+            NotValidFontExtension: not valid extension
+        """
         if font_family:
             if not os.path.isfile(font_family):
                 raise FontNotFound("Font not found")
 
-            if not font_family.endswith("ttf") and not font_family.endswith("otf"):
+            suffixes = ("ttf", "otf")
+            if not font_family.endswith(suffixes):
                 raise NotValidFontExtension("Only ttf and otf extension"
                                             "is supported")
